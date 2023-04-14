984f5ad773becf8a7ba3ddb4ad325fe26fa3f636
Manasa sadasiva <manasasadasiva@gmail.com>
2023-04-13 22:53:15 -0700

    day03
    addfuntion

---
+++ src/day02/Program_1.java
@@ -0,0 +1,25 @@
+package day02;
+
+public class Program_1 {
+
+	public static void main(String[] args) {
+		addFunction(10,20);
+		addFunction(30,40);
+		addFunction(20,40);
+		addFunction(50,60);
+		addFunction(60,80);
+	}
+	
+	public static void addFunction(int a,int b)
+	{
+		System.out.println(a+b);
+	}
+	
+
+}
+   
+    
+    
+    
+
+
---
+++ src/day03/Program01.java
@@ -0,0 +1,17 @@
+package day03;
+
+public class Program01 {
+
+	public static void main(String[] args) {
+	addFunction (10,20);
+	addFunction (10,20);
+	addFunction (10,20);
+	addFunction (10,20);
+	addFunction (10,20);
+	}		
+		public static void addFunction(int a,int b)
+		{
+			System.out.println(a+b);
+		}
+	}
+
